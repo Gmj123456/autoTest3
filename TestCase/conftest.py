8d8842c352d8e2075d2125135195cdc36fe71e14
Gmj123456 <1908280383@qq.com>
2025-04-30 15:22:03 +0800

    Auto commit

--- TestCase/conftest.py
+++ TestCase/conftest.py
@@ -1,18 +1,15 @@
 import pytest
-import requests
 import logging
-from utils.logger import setup_logging
-import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from pages.login_page import LoginPage
-from config.config import USERNAME, PASSWORD, PTUSER_USERNAME, PTUSER_PASSWORD, GETMENU
+from Base.base_page import BasePage
+from Base.config import USERNAME, PASSWORD
 import datetime
 
-# 调用统一的日志配置
-logger = setup_logging()
+# # 调用统一的日志配置
+# logger = setup_logging()
 
-from config.config import CHROME_DRIVER_PATH
+from Base.config import CHROME_DRIVER_PATH
 # 记录使用的 ChromeDriver 路径
 logging.info(f"使用的 ChromeDriver 路径: {CHROME_DRIVER_PATH}")
 
